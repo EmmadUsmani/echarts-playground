0d1e746adbdcf1bd5c8cb3091f3b9406c563475f
Emmad Usmani <emmadusmani@berkeley.edu>
2023-09-27 12:25:35 -0700

    simplify data

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,6 @@ const OPTION = {
       data: [
         { value: 1048, name: "Search Engine" },
         { value: 735, name: "Direct" },
-        { value: 580, name: "Email" },
-        { value: 484, name: "Union Ads" },
-        { value: 300, name: "Video Ads" },
       ],
     },
   ],
